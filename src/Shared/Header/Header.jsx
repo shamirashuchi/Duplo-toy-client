397354e40c07f9d2696382a2cab812b5064bfb3c
shamirashuchi <shamirashuchi@gmail.com>
2023-05-21 08:27:17 +0600

    color added

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         .catch(error => console.error(error))
     }
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar  bg-accent">
             <div className="navbar-start">
                 <div className="dropdown">
                 <label tabIndex={0} className="btn btn-ghost lg:hidden">
